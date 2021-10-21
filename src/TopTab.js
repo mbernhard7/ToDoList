2105df3c16fc70a18388e9e24988301cb4995321
Miles Bernhard <mbernhard22@students.claremontmckenna.edu>
2021-10-21 14:53:38 -0700

    Working on icons and edit screen

--- src/TopTab.js
+++ src/TopTab.js
@@ -1,28 +1,47 @@
 import './TopTab.css'
 import {AppModes} from "./App";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faEdit} from '@fortawesome/free-solid-svg-icons'
+import {faEdit, faSort, faPlus, faSave} from '@fortawesome/free-solid-svg-icons'
 
 function TopTab(props) {
     return <div id="topTab">
-        {
-            props.appMode === AppModes.EDIT_MODE ?
-                <button
-                    id="saveEdits"
-                    onClick={() => props.setAppMode(AppModes.DEFAULT_MODE)}
-                >Done</button>
-                : <button
-                    id="editButton"
-                    disabled={!props.existsTasks}
-                    onClick={() => props.setAppMode(AppModes.EDIT_MODE)}
-                ><FontAwesomeIcon icon={faEdit} /></button>
-        }
-        <h1 id="title">To-Do</h1>
-        <button id="addItem"
-                disabled={props.appMode !== AppModes.DEFAULT_MODE}
-                onClick={() => props.setAppMode(AppModes.ADD_MODE)}
-        >+
-        </button>
+        <div id='topTabThird'>
+            {
+                props.appMode === AppModes.EDIT_MODE ?
+                    <button
+                        id="saveEdits"
+                        onClick={() => props.setAppMode(AppModes.DEFAULT_MODE)}
+                    >
+                        <FontAwesomeIcon icon={faSave} size="xs" />
+                    </button>
+                    : <>
+                        <button
+                            id="editButton"
+                            disabled={!props.existsTasks}
+                            onClick={() => props.setAppMode(AppModes.EDIT_MODE)}
+                        >
+                            <FontAwesomeIcon icon={faEdit} size="xs" />
+                        </button>
+                        <button
+                            id="sortButton"
+                            onClick={() => props.setAppMode(AppModes.EDIT_MODE)}
+                        >
+                            <FontAwesomeIcon icon={faSort}/>
+                        </button>
+                    </>
+            }
+        </div>
+        <div id='topTabThird'>
+            <h1 id="title">To-Do</h1>
+        </div>
+        <div id='topTabThird'>
+            <button id="addItem"
+                    disabled={props.appMode !== AppModes.DEFAULT_MODE}
+                    onClick={() => props.setAppMode(AppModes.ADD_MODE)}
+            >
+                <FontAwesomeIcon icon={faPlus}/>
+            </button>
+        </div>
     </div>
 }
 
