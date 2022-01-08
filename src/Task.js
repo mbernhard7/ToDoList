78352b33ede921608e88dba1487e75b48be7ea39
Miles Bernhard <me@milesbernhard.com>
2022-01-08 13:39:14 -0500

    Revert "Lab3"

--- src/Task.js
+++ src/Task.js
@@ -1,29 +1,10 @@
 import './Task.css'
-import {AppModes} from "./SignedInApp";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrash} from '@fortawesome/free-solid-svg-icons'
-
+import {AppModes} from "./App";
 
 function Task(props) {
-
-
-    function addUpdateToChangeList(field, value) {
-        props.addToTaskChangeList(props.task.id, {...props.taskChangeList, [field]: value})
-    }
-
-    function getPriorityClass(num) {
-        if (num === 1) {
-            return 'priorityLevel low';
-        } else if (num === 2) {
-            return 'priorityLevel medium';
-        } else {
-            return 'priorityLevel high';
-        }
-    }
-
-    return <>
-        {!('delete' in props.taskChangeList) && <li className="listItem">
-            <label className="taskLabel">
+    return (
+        <li className={props.appMode === AppModes.EDIT_MODE ? "listItem editInProgress" : "listItem"}>
+            <label>
                 <input
                     className="checkbox"
                     type="checkbox"
@@ -31,51 +12,27 @@ function Task(props) {
                     disabled={props.appMode === AppModes.EDIT_MODE}
                     onChange={(e) => props.onTaskChanged(props.task.id, 'isChecked', e.target.checked)}
                 />
-                <div className="taskContent">
-                    {props.appMode === AppModes.EDIT_MODE ?
-                        <>
-                            <input
-                                className="editInput"
-                                type="text"
-                                value={props.taskChangeList.taskName !== undefined ?
-                                    props.taskChangeList.taskName : props.task.taskName}
-                                onChange={(e) => {
-                                    addUpdateToChangeList('taskName', e.target.value)
-                                }}
-                            />
-                            <select
-                                className='prioritySelector'
-                                value={props.taskChangeList?.priorityLevel || props.task.priorityLevel}
-                                onChange={(e) =>
-                                    addUpdateToChangeList('priorityLevel', parseInt(e.target.value))
-                                }
-                            >
-                                <option aria-label="Low priority" value='1'>!</option>
-                                <option aria-label="Medium priority" value='2'>!!</option>
-                                <option aria-label="High priority" value='3'>!!!</option>
-                            </select>
-                            <button
-                                aria-label="Delete task"
-                                className="deleteButton"
-                                onClick={() => {
-                                    addUpdateToChangeList('delete', true)
-                                }}
-                            >
-                                <FontAwesomeIcon icon={faTrash} size="xs"/>
-                            </button>
-                        </>
-                        :
-                        <>
-                            <span className="taskName">{props.task.taskName}</span>
-                            <span className={getPriorityClass(props.task.priorityLevel)}>
-                                {"!".repeat(props.task.priorityLevel)}
-                            </span>
-                        </>
-                    }
-                </div>
+                {props.appMode !== AppModes.EDIT_MODE && <span>{props.task.taskName}</span>}
             </label>
-        </li>}
-    </>
+            {props.appMode === AppModes.EDIT_MODE &&
+            <>
+                <label>
+                    <input
+                        className="editInput"
+                        type="text"
+                        value={props.task.taskName}
+                        onChange={(e) => props.onTaskChanged(props.task.id, 'taskName', e.target.value)}
+                    />
+                </label>
+                <button
+                    className="deleteButton"
+                    onClick={() => props.onTasksDeleted([props.task.id])}
+                >Delete
+                </button>
+            </>
+            }
+        </li>
+    )
 }
 
 export default Task;
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,36 +1,27 @@
 import './TaskList.css'
 import Task from "./Task";
-import {AppModes, TasksShowing} from "./SignedInApp";
+import {AppModes, TasksShowing} from "./App";
 
 function TaskList(props) {
     let tasks = props.data?.filter(task =>
         props.tasksShowing === TasksShowing.ALL ||
         (props.tasksShowing === TasksShowing.UNCOMPLETED && !task.isChecked)
     );
-
-    function addToTaskChangeList(id, changes) {
-        props.setDataChanges({...props.dataChanges, [id]: changes})
-    }
-
     return <ul id="itemList">
         {tasks.length > 0 ?
             tasks.map(task => {
                 return <Task
                     key={task.id}
                     task={task}
-                    onTaskChanged={props.onTaskChanged}
                     appMode={props.appMode}
-                    addToTaskChangeList={addToTaskChangeList}
-                    taskChangeList={task.id in props.dataChanges ? props.dataChanges[task.id] : {}}
+                    onTaskChanged={props.onTaskChanged}
+                    onTasksDeleted={props.onTasksDeleted}
                 />
             })
             : <li key='1' id="noItems">
                 <h3>{props.appMode === AppModes.EDIT_MODE ?
                     'No tasks to edit.' :
-                    props.lists.length === 0 ?
-                        'You have no lists. Create your first one by clicking the lists icon above!'
-                        : 'You have completed all your tasks, woohoo!'
-                }
+                    'You have completed all your tasks, woohoo!'}
                 </h3>
             </li>
         }
